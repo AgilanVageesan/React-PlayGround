66e7268e5fece3e7def745e218150aa0d4b3414c
Agilan Vageesan <agilanvageesan@gmail.com>
2021-04-03 16:55:34 +0530

    Title Change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Here We Go Amigos</h1>
+        <h1>Welcome to React Playground</h1>
       </header>
     </div>
   );
